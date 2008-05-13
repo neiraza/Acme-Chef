e422335f99124e5ef9656f72832641ecb80afaf9
Steffen Mueller <smueller@cpan.org>
2008-05-13 01:40:20 -0800

    import Acme::Chef 1.01 from CPAN
    git-cpan-module: Acme::Chef
    git-cpan-version: 1.01
    git-cpan-authorid: SMUELLER

--- lib/Acme/Chef.pm
+++ lib/Acme/Chef.pm
@@ -10,7 +10,7 @@ use warnings;
 use Carp;
 
 use vars qw/$VERSION/;
-$VERSION = '1.00';
+$VERSION = '1.01';
 
 use Acme::Chef::Recipe;
 use Acme::Chef::Container;
@@ -398,7 +398,7 @@ method instructions are:
 
 =item *
 
-C<Take ingredient from refridgerator.>
+C<Take ingredient from refrigerator.>
 
 I<New specification!> This reads lines from STDIN until a
 numerical value is found. This numerical value is put into the
@@ -604,9 +604,9 @@ Steffen Mueller.
 
 Chef was designed by David Morgan-Mar.
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2002-2005 Steffen Mueller. All rights reserved. This program is
+Copyright (c) 2002-2008 Steffen Mueller. All rights reserved. This program is
 free software; you can redistribute it and/or modify it under the same
 terms as Perl itself.
 
--- lib/Acme/Chef/Ingredient.pm
+++ lib/Acme/Chef/Ingredient.pm
@@ -189,9 +189,9 @@ Steffen Mueller.
 
 Chef designed by David Morgan-Mar.
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2002-2005 Steffen Mueller. All rights reserved. This program is
+Copyright (c) 2002-2008 Steffen Mueller. All rights reserved. This program is
 free software; you can redistribute it and/or modify it under the same
 terms as Perl itself.
 
